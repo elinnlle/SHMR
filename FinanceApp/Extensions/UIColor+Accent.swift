e6e7d9e63271eaa4baa115dd58d1261616072f5b
elinnlle <matveenkoelvira307@gmail.com>
2025-07-24 18:48:35 +0300

    Реализовано дополнительное задание. Добавлен extension UIColor, чтобы UIKit снова видел AccentColor.

---
+++ FinanceApp/Extensions/UIColor+Accent.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Accent.swift
+//  FinanceApp
+//
+//  Created by Эльвира Матвеенко on 24.07.2025.
+//
+
+import UIKit
+
+extension UIColor {
+    static var accent: UIColor {
+        // Для приложения:
+        return UIColor(named: "AccentColor")!
+    }
+}
